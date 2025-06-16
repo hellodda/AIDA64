8d40f8ef7ff1e0376312e6e7fadb5ce67255f3c7
CodeOtakuu <notbukaaa777@gmail.com>
2025-06-16 14:35:10 +0400

    Добавьте файлы проекта.

---
+++ AIDA64.BLL/WMIComponentObject.cpp
@@ -0,0 +1,10 @@
+﻿#include "pch.h"
+#include "WMIComponentObject.h"
+#if __has_include("WMIComponentObject.g.cpp")
+#include "WMIComponentObject.g.cpp"
+#endif
+
+namespace winrt::AIDA64_BLL::implementation
+{
+ 
+}
---
+++ AIDA64.BLL/WMIComponentObject.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "WMIComponentObject.g.h"
+
+namespace winrt::AIDA64_BLL::implementation
+{
+    struct WMIComponentObject : WMIComponentObjectT<WMIComponentObject>
+    {
+        WMIComponentObject() = default;
+
+    };
+}
+
+namespace winrt::AIDA64_BLL::factory_implementation
+{
+    struct WMIComponentObject : WMIComponentObjectT<WMIComponentObject, implementation::WMIComponentObject>
+    {
+    };
+}
